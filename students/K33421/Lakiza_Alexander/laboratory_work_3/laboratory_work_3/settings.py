d55156fe3e2c4da50f469d85e23cab2312104e78
alexanderlakiza <alelakiza@yandex.ru>
2022-06-07 20:51:50 +0300

    change db to postgresql

--- students/K33421/Lakiza_Alexander/laboratory_work_3/laboratory_work_3/settings.py
+++ students/K33421/Lakiza_Alexander/laboratory_work_3/laboratory_work_3/settings.py
@@ -90,10 +90,22 @@ WSGI_APPLICATION = 'laboratory_work_3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'college_db',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'college_db'
     }
 }
 
